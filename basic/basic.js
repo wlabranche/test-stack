a1ca3dd757537f8ef6130c57cba6181e25714759
Nick Bottomley <nhbottomley@gmail.com>
2014-07-30 19:33:16 -0700

    Tests working locally

---
+++ basic/basic.js
@@ -0,0 +1,16 @@
+module.exports = ( function () {
+
+  var addNumbers = function ( a, b ) {
+    return a + b;
+  }
+
+  var reverseString = function ( str ) {
+    return str.split( '' ).reverse().join( '' );
+  }
+
+  return {
+    add: addNumbers,
+    reverse: reverseString
+  };
+
+})();
