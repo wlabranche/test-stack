100c8d868cc304050692d8d30b7c394c1c01d522
Will LaBranche <wlabranche@gmail.com>
2014-07-30 19:46:08 -0700

    fixed

--- basic/basic.js
+++ basic/basic.js
@@ -1,12 +1,12 @@
 module.exports = (function () {
 
   var addNumbers = function ( a, b ) {
-    return a - b;
-  }
+    return a + b;
+  };
 
   var reverseString = function ( str ) {
     return str.split( '' ).reverse().join( '' );
-  }
+  };
 
   return {
     add: addNumbers,
