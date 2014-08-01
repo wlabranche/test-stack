24dc1290c55143068a25f417b43dffe164565578
Will LaBranche <wlabranche@gmail.com>
2014-08-01 15:58:26 -0700

    upadted to handle multiple tests, hold test files with src files

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+var mocha = require('gulp-mocha');
+// var phantom = require('gulp-phantom');
+
+var istanbul = require('gulp-istanbul');
+
+gulp.task('mochaTest', function(cb){
+  gulp.src('./basic/*.js')
+    .pipe(istanbul())
+    .on('finish', function(){
+      gulp.src(['./basic/*-spec.js'])
+        .pipe(mocha({reporter: 'spec'}))
+        .pipe(istanbul.writeReports())
+        .on('end', cb);
+    });
+});
+
+gulp.task('test', ['mochaTest']);
+
+gulp.task('default', ['test']);
