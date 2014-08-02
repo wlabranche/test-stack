a879d6dc1219121c66a6374fee052959d37e83b4
Will LaBranche <wlabranche@gmail.com>
2014-08-01 19:13:08 -0700

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,24 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
-var mocha = require('gulp-mocha');
+// var spawn = require('child_process').spawn;
 // var phantom = require('gulp-phantom');
-
 var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
+var ghostCoffee = require('gulp-mocha-phantomjs');
 
-gulp.task('mochaTest', function(cb){
-  gulp.src('./basic/**/!(*-spec.js)+(*.js)')
+gulp.task('test', function(cb){
+  gulp.src(['./basic/**/!(*-spec.js)+(*.js)', './dom/**/!(*-spec.js)+(*.js)', './backbone/**/!(*-spec.js)+(*.js)'])
     .pipe(istanbul())
     .on('finish', function(){
-      gulp.src(['./basic/*-spec.js'])
+      gulp.src(['./basic/*-spec.js', './dom/*-spec.js]'])
         .pipe(mocha({reporter: 'spec'}))
+        .pipe(ghostCoffee({reporter: 'spec'}))
         .pipe(istanbul.writeReports())
         .on('end', cb);
     });
 });
 
 
-gulp.task('test', ['mochaTest']);
 
 gulp.task('default', ['test']);
