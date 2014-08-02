366dc690c79578f3ab991b5225f2319152df03c3
Will LaBranche <wlabranche@gmail.com>
2014-08-01 19:48:16 -0700

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,25 @@ var gutil = require('gulp-util');
 // var phantom = require('gulp-phantom');
 var istanbul = require('gulp-istanbul');
 var mocha = require('gulp-mocha');
-var ghostCoffee = require('gulp-mocha-phantomjs');
+var mochaPhantomJS = require('gulp-mocha-phantomjs');
+var lcov = require('mocha-lcov-reporter');
 
 gulp.task('test', function(cb){
-  gulp.src(['./basic/**/!(*-spec.js)+(*.js)', './dom/**/!(*-spec.js)+(*.js)', './backbone/**/!(*-spec.js)+(*.js)'])
+  gulp.src(['./basic/**/!(*-spec.js)+(*.js)', './dom/**/!(*-spec.js)+(*.js)'])
     .pipe(istanbul())
     .on('finish', function(){
       gulp.src(['./basic/*-spec.js', './dom/*-spec.js]'])
         .pipe(mocha({reporter: 'spec'}))
-        .pipe(ghostCoffee({reporter: 'spec'}))
+        // .pipe(mochaPhantomJS({reporter: 'spec'}))
         .pipe(istanbul.writeReports())
         .on('end', cb);
     });
 });
 
+gulp.task('backbone', function(cb){
+  gulp.src('./backbone/test.html')
+    .pipe(mochaPhantomJS({reporter: 'spec'}));
+});
 
 
-gulp.task('default', ['test']);
+gulp.task('default', ['test', 'backbone']);
